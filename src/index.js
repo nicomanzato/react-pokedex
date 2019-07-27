dcca24bb91ffbd3db0c2e28ccca31b49f37274b2
Nicolas Manzato <nicolasmanzato@MacBook-Pro-de-Nicolas.local>
2019-07-27 10:27:55 -0300

    font changed to montserrat

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'normalize.css';
+import 'typeface-montserrat';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import * as serviceWorker from './serviceWorker';
